fa2d22c459aa2670bd9f7b02a6b3eb235d41b018
Al <moebiusproject@gmail.com>
2017-05-30 21:41:35 +1200

    Restrict classes to ones we know fit in

--- code/dataobjects/BootStrapRow.php
+++ code/dataobjects/BootStrapRow.php
@@ -28,7 +28,7 @@ class BootStrapRow extends Block{
 	 * @return array
 	 */
 	public function getBootStrapBlockClasses(){
-		$classes = ArrayLib::valuekey(ClassInfo::subclassesFor('Block'));
+		$classes = ArrayLib::valuekey(ClassInfo::subclassesFor('BootStrapBlock'));
 
 		foreach ($classes as $k => $v) {
 			$classes[$k] = singleton($k)->singular_name();
