71f92fbe964e73cc0db802bb1161e67f58bcb630
Al Twohill <al@twohill.nz>
2019-03-04 19:24:35 +1300

    Fixes for SS4 Compat

--- _config.php
+++ _config.php
@@ -1,6 +1,1 @@
 <?php
-//define global path to Components' root folder
-use SilverStripe\Admin\LeftAndMain;
-use SilverStripe\Core\Config\Config;
-
-Config::inst()->modify()->set(LeftAndMain::class,'extra_requirements_javascript', array("sheadawson/silverstripe-blocks: javascript/blocks-cms.js"));
