5bc06af0cfabe463ae39f71667ae3b77e50c4f09
Al Twohill <al@twohill.nz>
2019-03-04 20:19:31 +1300

    Fixes for SS4 Compat

--- code/controllers/BlockAdmin.php
+++ code/controllers/BlockAdmin.php
@@ -51,23 +51,23 @@ class BlockAdmin extends ModelAdmin
     }
 
 
-    /**
-     * @return Form
-     **/
-    public function getEditForm($id = null, $fields = null)
-    {
-        Versioned::reading_stage('Stage');
-        $form = parent::getEditForm($id, $fields);
-
-        if ($blockGridField = $form->Fields()->fieldByName('Block')) {
-            $blockGridField->setConfig(GridFieldConfig_BlockManager::create(true, true, false));
-            $config = $blockGridField->getConfig();
-            $dcols = $config->getComponentByType('GridFieldDataColumns');
-            $dfields = $dcols->getDisplayFields($blockGridField);
-            unset($dfields['BlockArea']);
-            $dcols->setDisplayFields($dfields);
-        }
-
-        return $form;
-    }
+//    /**
+//     * @return Form
+//     **/
+//    public function getEditForm($id = null, $fields = null)
+//    {
+//        Versioned::set_reading_mode('Stage');
+//        $form = parent::getEditForm($id, $fields);
+//
+//        if ($blockGridField = $form->Fields()->fieldByName('Block')) {
+//            $blockGridField->setConfig(GridFieldConfig_BlockManager::create(true, true, false));
+//            $config = $blockGridField->getConfig();
+//            $dcols = $config->getComponentByType('GridFieldDataColumns');
+//            $dfields = $dcols->getDisplayFields($blockGridField);
+//            unset($dfields['BlockArea']);
+//            $dcols->setDisplayFields($dfields);
+//        }
+//
+//        return $form;
+//    }
 }
