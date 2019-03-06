7f71bc8cbdfe50e5bed3c880241e792369f827dc
Al Twohill <al@twohill.nz>
2019-03-06 13:52:07 +1300

    Fixes for SS4 Compat

--- code/dataobjects/BootStrapBlock.php
+++ code/dataobjects/BootStrapBlock.php
@@ -10,48 +10,61 @@ use SilverStripe\Core\ClassInfo;
 /**
  * BootStrapBlocks are very similar to Blocks, but they live inside BootStrapRows
  */
-class BootStrapBlock extends DataObject{
+class BootStrapBlock extends DataObject
+{
 
-	private static $singular_name = 'Block';
-	private static $plural_name = 'Blocks';
+    private static $singular_name = 'Block';
+    private static $plural_name = 'Blocks';
 
-	private static $table_name = "BootStrapBlock";
+    private static $table_name = "BootStrapBlock";
 
-	private static $db = array(
-		'Title' => 'Varchar(255)',
-		'ShowTitle' => 'Boolean',
-		'BootStrapColumnClass' => 'Varchar(20)',
-		'Content' => 'HTMLText',
+    public function getShortClassName()
+    {
+        try {
+            return (new \ReflectionClass($this))->getShortName();
+        } catch (\ReflectionException $e) {
+        }
+    }
+
+    private static $db = array(
+        'Title' => 'Varchar(255)',
+        'ShowTitle' => 'Boolean',
+        'BootStrapColumnClass' => 'Varchar(20)',
+        'Content' => 'HTMLText',
         'Sort' => 'Int'
-	);
+    );
 
-	private static $has_one = array(
-		'BootStrapRow' => BootStrapRow::class,
-	);
+    private static $has_one = array(
+        'BootStrapRow' => BootStrapRow::class,
+    );
 
 
     private static $default_sort = array('Sort' => 'ASC');
 
 
-    public function getCSSClass() {
-		$filter = URLSegmentFilter::create();
-		return $filter->filter($this->Title);
-	}
+    public function getCSSClass()
+    {
+        $filter = URLSegmentFilter::create();
+        return $filter->filter($this->Title);
+    }
 
-	/**
-	 * Traverses up the class ancestry looking for templates
-	 * If you inherit from BootStrap block you can choose to create a template for the class name
-	 *
-	 * @return String the formatted block
-	 */
-	public function getFormattedBlock() {
-		$matches = array();
+    /**
+     * Traverses up the class ancestry looking for templates
+     * If you inherit from BootStrap block you can choose to create a template for the class name
+     *
+     * @return String the formatted block
+     */
+    public function getFormattedBlock()
+    {
+        $matches = array();
 
-		foreach(array_reverse(ClassInfo::ancestry($this)) as $className) {
-			$matches[] = $className;
+        foreach (array_reverse(ClassInfo::ancestry($this)) as $className) {
+            $matches[] = $className;
 
-			if($className == "BootStrapBlock") break;
-		}
-		return $this->renderWith($matches);
-	}
+            if ($className == "BootStrapBlock") {
+                break;
+            }
+        }
+        return $this->renderWith($matches);
+    }
 }
--- templates/Twohill/Legacy/dataobjects/BootStrapRow.ss
+++ templates/Twohill/Legacy/dataobjects/BootStrapRow.ss
@@ -1,7 +1,7 @@
 <section class="$EvenOdd $CSSClass <% if $ShowDivider %>divider<% end_if %>">
         <div class="row">
             <% loop $BootStrapBlocks %>
-                <div class="$BootStrapColumnClass $CSSClass $ClassName">$FormattedBlock</div>
+                <div class="$BootStrapColumnClass $CSSClass $ShortClassName">$FormattedBlock</div>
             <% end_loop %>
         </div>
 </section>
